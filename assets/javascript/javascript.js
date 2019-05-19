d6cd72c9686bc10c78ec9285ef07f21b109cd47f
skish1987 <seankish1@gmail.com>
2019-05-18 21:00:49 -0700

    frame set

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,24 @@
-javs
+
+var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r'", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var guesslefttext= document.getElementById("guess-left-text");
+var guesslogtext= document.getElementById("guess-log-text");
+var winstext= document.getElementById("wins-text");
+var lossestext=document.getElementById("losses-text");
+
+var wins = 0;
+var losses = 0;
+var guessleft = 9;
+
+var computerGuess = letterArray[Math.floor(Math.random() * 26)];
+
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+ 
+
+    guesslogtext.textContent = "Your guesses so far: " + userGuess}
+
+    // guessleft: function() {
+    
+    //     this.guessleft = this.guessleft - 1

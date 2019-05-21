57093d1f9eb4bde4ac24b5eabcfa66bb91fcb273
skish1987 <seankish1@gmail.com>
2019-05-21 15:07:25 -0700

    reset function made

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,43 +1,48 @@
 
 var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r'", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+
 var guesslefttext= document.getElementById("guess-left-text");
 var guesslogtext= document.getElementById("guess-log-text");
 var winstext= document.getElementById("wins-text");
 var lossestext=document.getElementById("losses-text");
 
+
+function reset() {
+    var wins = 0;
+    alert("GAME OVER");
+    var losses = 0;
+    var guessleft = 10;
+    var userGuesses =[];
+    winstext.innerHTML = "Wins: " + wins;
+    lossestext.innerHTML = "Losses: " + losses
+    guesslefttext.innerHTML = "Attempts Remaining: " + guessleft;  
+    guesslogtext.innerHTML = "Your guesses so far: "
+}
 var wins = 0;
 var losses = 1;
 var guessleft = 10;
-
-
 var computerGuess = letterArray[Math.floor(Math.random() * 26)];
 var userGuesses = [];
-
-
-// for (var i=0; i < 9; i++) {
-    document.onkeyup = function(event) {
+document.onkeyup = function(event) {
     var userGuess = event.key;
-    
-    userGuesses.push(userGuess);
+    userGuesses.push(" " + userGuess);
     guessleft --;
-    
     guesslefttext.innerHTML = "Attempts Remaining: " + guessleft;
-    
     guesslogtext.innerHTML = "Your guesses so far: " + userGuesses;
-    console.log(userGuesses)
-
-    winstext.innerHTML = "wins: " + wins;
-    lossestext.innerHTML  = "losses: " + losses;
-    console.log(guessleft);
-    console.log(computerGuess);
-    console.log(userGuess);
+ 
+    winstext.innerHTML = "Wins: " + wins;
+    lossestext.innerHTML  = "Losses: " + losses;
+    
     if (userGuess === computerGuess) {
-    wins ++;
-    // winstext.textcontent = "Wins: " + this.wins
-}
-else {
-    losses ++;
-    // lossestext.textcontent = "losses: " + this.losses
-}
-}
+        wins ++;
+    }
+    else {
+        losses ++;
+    
+    }
+    
+// If no guesses left
+if (guessleft == 0) {
+   reset();
+   }}
